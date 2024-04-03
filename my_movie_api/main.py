72bac02c2ba38bdf763b36bf3ccdb0758b1d8e05
Juan Amezquita <60975619+Juandaamez@users.noreply.github.com>
2024-04-03 18:18:34 -0500

    Final_course

--- my_movie_api/main.py
+++ my_movie_api/main.py
@@ -1,47 +1,24 @@
-from fastapi import Body, Depends, FastAPI, HTTPException, Path, Query, Request
+from fastapi import FastAPI
 from fastapi.responses import HTMLResponse, JSONResponse
-from fastapi.security.http import HTTPAuthorizationCredentials
-from pydantic import BaseModel, Field
-from typing import Coroutine, Optional, List
-from jwt_manager import create_token, validate_token
-from fastapi.security import HTTPBearer
+from pydantic import BaseModel
+from utils.jwt_manager import create_token
+from config.database import engine, Base
+from middlewares.error_handler import ErrorHandler
+from routers.movie import movie_router
+
 
 app = FastAPI()
 app.title = "Mi aplicacion con FastAPI"
 app.version = "0.0.1"
 
-class JWTBearer(HTTPBearer):
-    async def __call__(self, request: Request):
-        auth = await super().__call__(request)
-        data = validate_token(auth.credentials)
-        if data['email'] != "admin@gmail.com":
-            raise HTTPException(status_code=403, detail="Credenciales son invalidas")
+app.add_middleware(ErrorHandler)
+app.include_router(movie_router)
+
+Base.metadata.create_all(bind=engine)
 
 class User(BaseModel):
     email: str
-    password: str
-
-
-class Movie(BaseModel):
-    id: Optional[int] = None
-    title: str = Field(min_length= 5, max_length=15)
-    overview: str = Field(min_length= 5, max_length=15)
-    year: int = Field(le=2022)
-    rating: float = Field(ge=1, le=10)
-    category: str = Field(min_length= 5, max_length=15)
-    
-    class Config:
-        schema_extra = {
-            "example": {
-                "id": 1,
-                "title": "Mi pelicula",
-                "overview": "Descripcion de la pelicula",
-                "year": 2022,
-                "rating": 9.8,
-                "category": "Accion"
-			}
-		}
-    
+    password: str  
 
 movies = [
     {
@@ -71,42 +48,3 @@ def login(user: User):
     if user.email == "admin@gmail.com" and user.password == "admin":
         token : str = create_token(user.dict())
     return JSONResponse(content = token, status_code=200)
-
-@app.get('/movies', tags=['movies'], response_model=List[Movie], status_code=200, dependencies=[Depends(JWTBearer())])
-def get_movies() -> List["Movie"]:
-    return JSONResponse(status_code=200, content=movies)
-
-@app.get('/movies/{id}', tags=['movies'], response_model=List[Movie], status_code=200)
-def get_movie(id: int = Path(ge = 1, le = 2000))  -> Movie:
-    for item in movies:
-        if item["id"] == id:
-            return JSONResponse(content=item)
-    return JSONResponse(status_code=200, content=[])
-
-''''@app.get('/movies', tags=['movies'], response_model=List[Movie])
-def get_movies_by_category(category: str = Query(min_length=5, max_length=15)) -> List["Movie"]:
-    data = [item for item in movies if item['category'] == category ]
-    return JSONResponse(content=data)'''
-
-@app.post('/movies', tags=['movies'], response_model=dict, status_code=201)
-def create_movie(movie: Movie) -> dict:
-    movies.append(movie)
-    return JSONResponse(status_code=201, content={"message": "Se registró la pelicula"})
-
-@app.put('/movies/{id}', tags=['movies'], response_model=dict, status_code=200)
-def update_movie(id: int, movie: Movie) -> dict:
-    for item in movies:
-        if item["id"] == id:
-            item['title'] = movie.title
-            item['overview'] = movie.overview
-            item['year'] = movie.year
-            item['rating'] = movie.rating
-            item['category'] = movie.category
-            return JSONResponse(status_code=200, content={"message": "Se ha modificado la pelicula"})
-        
-@app.delete('/movies/{id}', tags=['movies'], response_model=dict, status_code=200)
-def delete_movie(id: int) -> dict:
-    for item in movies:
-        if item["id"] == id:
-            movies.remove(item)
-            return JSONResponse(status_code=200, content={"message": "Se ha eliminado la pelicula"})
